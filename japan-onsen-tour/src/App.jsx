d280ab878ccbfe071e2aacf638798cd93b9c47a4
SKim <45351992+elriot@users.noreply.github.com>
2025-05-26 07:59:21 -0700

    Added favicon, images
    - changed content image layout
    desktop : 1row 2 column
    mobile : 1row 1column

--- japan-onsen-tour/src/App.jsx
+++ japan-onsen-tour/src/App.jsx
@@ -1,44 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import Header from "./pages/Header";
+import MainPage from "./pages/WhatIsOnsen";
+import "/src/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <p className="text-2xl font-bold text-blue-600 underline">Hello, Tailwind!</p>
-      
-      <div className="card bg-green-200 p-4 rounded shadow-md">
-        <button 
-          onClick={() => setCount((count) => count + 1)} 
-          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700"
-        >
-          count is {count}
-        </button>
-      </div>
-
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+		<Header/>
+		<MainPage/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
