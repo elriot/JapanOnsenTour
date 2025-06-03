8fe6fc5f212055a5bc849a7bffa89f4bfecf4c3b
SKim <45351992+elriot@users.noreply.github.com>
2025-06-02 17:15:02 -0700

    mobile view updated

--- japan-onsen-tour/src/components/TransfortationInfoBox.jsx
+++ japan-onsen-tour/src/components/TransfortationInfoBox.jsx
@@ -6,7 +6,7 @@ function TransportationInfoBox( {title, subtitle, description, rightContent, cla
     <Section className={`${className}`} paddingBottom="false" paddingTop="false">
       <div className="flex flex-col bg-gray-50 border-3 border-gray-200 rounded-lg w-full md:flex-row ">
         {/* left section (1/3) */}
-        <div className="md:w-1/3 p-10 page-content-text space-y-2">
+        <div className="md:w-1/3 p-5 md:p-10 page-content-text space-y-2">
           <h2 className="text-xl font-bold">{title}</h2>
           <p>{subtitle}</p>
           <p>
@@ -15,7 +15,7 @@ function TransportationInfoBox( {title, subtitle, description, rightContent, cla
         </div>
 
         {/* right section (2/4) */}
-        <div className="md:w-2/3 px-10 md:py-10 pb-10 page-content-text space-y-2">
+        <div className="md:w-2/3 p-5 md:p-10 md:py-10 pb-10 page-content-text space-y-2">
           {rightContent}
         </div>
       </div>
